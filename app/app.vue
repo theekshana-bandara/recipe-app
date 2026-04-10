0399a39d969238905da66b389a8bd7edc645e0ef
Theekshana Bandara <Theekshana.Bandara@intrepidtravel.com>
2026-04-10 11:46:43 +0530

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="font-Montserrat container">
+    <h1 class="text-lg text-blue-400">Hello</h1>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "dodgeroll-gold": {
+          50: "#fff9eb",
+          100: "#fdecc8",
+          200: "#fbd88c",
+          300: "#f9bd50",
+          400: "#f79f1a",
+          500: "#f1820f",
+          600: "#d65f09",
+          700: "#b13f0c",
+          800: "#903210",
+          900: "#762911",
+          950: "#441204",
+        },
+        "apple-green": "#046E1B",
+        "dire-wolf": "#292727",
+      },
+      fontFamily: {
+        Montserrat: "Montserrat, sans-serif",
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+    },
+  },
+  plugins: [],
+}
+
