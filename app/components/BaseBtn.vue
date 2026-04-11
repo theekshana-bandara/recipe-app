a25395505e464842d79a505265a244194125e854
theekshana-bandara <Theekshana.Bandara@intrepidtravel.com>
2026-04-12 00:09:09 +0530

    Add BaseBtn component and refactor RecipeCard to use it for navigation

---
+++ app/components/BaseBtn.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  label?: string;
+  to?: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="to"
+    class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
+  >
+    <slot>{{ label }}</slot>
+  </NuxtLink>
+</template>
+
+<style lang="scss" scoped></style>
--- app/components/RecipeCard.vue
+++ app/components/RecipeCard.vue
@@ -34,12 +34,7 @@ defineProps<{
           <span>{{ recipe.rating }} ({{ recipe.reviewCount }})</span>
         </div>
       </div>
-      <NuxtLink
-        :to="`/recipes/${recipe.id}`"
-        class="px-4 py-2 text-white self-start bg-dodgeroll-gold rounded-md text-base lg:text-lg cursor-pointer"
-      >
-        View
-      </NuxtLink>
+      <BaseBtn :to="`recipes/${recipe.id}`" label="View" />
     </div>
   </div>
 </template>
